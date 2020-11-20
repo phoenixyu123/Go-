cacdc6743899e6b2ef6e90553f8ffae448c49c00
PhoenixYu123 <1565636387@qq.com>
2020-11-20 17:03:28 +0800

    master

---
+++ SGG/chapter14/flagdemo/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	//定义变量用于接收命令行参数值
+	var user string
+	var pwd string
+	var host string
+	var port int
+
+	//&user 就是接受用户命令行中输入的-u后面的参数值
+	//"u" 就是-u指定参数
+	//"" 默认值
+	//说明
+	flag.StringVar(&user, "u", "", "用户名默认为空")
+	flag.StringVar(&pwd, "pwd", "", "密码默认为空")
+	flag.StringVar(&host, "h", "localhost", "主机名，默认为localhost")
+	flag.IntVar(&port, "port", 3306, "端口号，默认为3306")
+
+	//非常重要的操作，转换，必须调用该方法
+	flag.Parse()
+
+	//输出
+	fmt.Printf("user = %v pwd = %v host = %v port = %v\n", user, pwd, host, port)
+}
